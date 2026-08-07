72512beec41728f740f821779a2263cfb5a8d96d
Anirudh <anirudhsrk07@gmail.com>
2026-08-07 21:24:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 My First Hackathon Project</h1>
+      <p>Frontend is working successfully!</p>
+    </div>
+  );
+}
+
+export default App; 
